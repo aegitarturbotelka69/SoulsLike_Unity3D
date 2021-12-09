4e943722da2b44270c6ded920110c55e2e7d7405
Timur <jeynado_farbi_ocimio_eceterial_j@yahoo.com>
2021-12-09 23:16:24 +1100

    StateMachine Idle&Move fixes
    - Fixed transition bugs
    - Fixed animation lock
    - Fixed input check

--- Assets/Scripts/CharacterStates/CharacterIdleState.cs
+++ Assets/Scripts/CharacterStates/CharacterIdleState.cs
@@ -12,17 +12,24 @@ namespace SLGame.Gameplay
         override public void OnStateUpdate(Animator animator, AnimatorStateInfo animatorStateInfo, int layerIndex)
         {
 
-            if (VirtualInputManager.Instance.MoveFront && VirtualInputManager.Instance.MoveBack)
-                return;
-
-            if (VirtualInputManager.Instance.MoveLeft && VirtualInputManager.Instance.MoveRight)
-                return;
-
-            if (VirtualInputManager.Instance.MoveFront || VirtualInputManager.Instance.MoveBack || VirtualInputManager.Instance.MoveLeft || VirtualInputManager.Instance.MoveRight)
+            if (VirtualInputManager.Instance.MoveFront && !VirtualInputManager.Instance.MoveBack)
+            {
+                animator.SetBool(States.Move.ToString(), true);
+            }
+            if (VirtualInputManager.Instance.MoveBack && !VirtualInputManager.Instance.MoveFront)
             {
                 animator.SetBool(States.Move.ToString(), true);
             }
 
+            if (VirtualInputManager.Instance.MoveLeft && !VirtualInputManager.Instance.MoveRight)
+            {
+                animator.SetBool(States.Move.ToString(), true);
+            }
+
+            if (VirtualInputManager.Instance.MoveRight && !VirtualInputManager.Instance.MoveLeft)
+            {
+                animator.SetBool(States.Move.ToString(), true);
+            }
         }
 
         override public void OnStateExit(Animator animator, AnimatorStateInfo animatorStateInfo, int layerIndex)
--- Assets/Scripts/CharacterStates/CharacterWalkState.cs
+++ Assets/Scripts/CharacterStates/CharacterWalkState.cs
@@ -12,29 +12,63 @@ namespace SLGame.Gameplay
 
         override public void OnStateUpdate(Animator animator, AnimatorStateInfo animatorStateInfo, int layerIndex)
         {
-            if (VirtualInputManager.Instance.MoveFront && !VirtualInputManager.Instance.MoveBack)
-                PlayerMovement.zAxis = 1f;
-            else if (VirtualInputManager.Instance.MoveBack && !VirtualInputManager.Instance.MoveFront)
-                PlayerMovement.zAxis = -1f;
-            else
-                PlayerMovement.zAxis = 0f;
-
-            if (VirtualInputManager.Instance.MoveRight && !VirtualInputManager.Instance.MoveLeft)
-                PlayerMovement.xAxis = 1f;
-            else if (VirtualInputManager.Instance.MoveLeft && !VirtualInputManager.Instance.MoveRight)
-                PlayerMovement.xAxis = -1f;
-            else
-                PlayerMovement.xAxis = 0f;
+            Apply_Z_AxisWalk(ref animator);
+            Apply_X_AxisWalk(ref animator);
 
             if (PlayerMovement.zAxis == 0f && PlayerMovement.xAxis == 0f)
-            {
                 animator.SetBool(States.Idle.ToString(), true);
-            }
         }
 
         override public void OnStateExit(Animator animator, AnimatorStateInfo animatorStateInfo, int layerIndex)
         {
             animator.SetBool(States.Move.ToString(), false);
+        }
+
+
+
+        private void Apply_Z_AxisWalk(ref Animator animator)
+        {
+            if (VirtualInputManager.Instance.MoveFront && VirtualInputManager.Instance.MoveBack)
+            {
+                PlayerMovement.zAxis = 0f;
+                return;
+            }
+            if (VirtualInputManager.Instance.MoveFront && !VirtualInputManager.Instance.MoveBack)
+            {
+                PlayerMovement.zAxis = 1f;
+                return;
+            }
+
+            if (VirtualInputManager.Instance.MoveBack && !VirtualInputManager.Instance.MoveFront)
+            {
+                PlayerMovement.zAxis = -1f;
+                return;
+            }
+
+            PlayerMovement.zAxis = 0f;
+        }
+
+        private void Apply_X_AxisWalk(ref Animator animator)
+        {
+            if (VirtualInputManager.Instance.MoveLeft && VirtualInputManager.Instance.MoveRight)
+            {
+                PlayerMovement.xAxis = 0f;
+                return;
+            }
+
+            if (VirtualInputManager.Instance.MoveLeft && !VirtualInputManager.Instance.MoveRight)
+            {
+                PlayerMovement.xAxis = -1f;
+                return;
+            }
+
+            if (VirtualInputManager.Instance.MoveRight && !VirtualInputManager.Instance.MoveLeft)
+            {
+                PlayerMovement.xAxis = 1f;
+                return;
+            }
+
+            PlayerMovement.xAxis = 0f;
         }
     }
 }
