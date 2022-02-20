832276b368635cd7cc88da3fd99913930999a346
Timur <jeynado_farbi_ocimio_eceterial_j@yahoo.com>
2022-02-21 06:03:42 +1100

    Ground Layer, Gravity Check

--- Assets/Main_Character/Scripts/PlayerGravityCheck.cs
+++ Assets/Main_Character/Scripts/PlayerGravityCheck.cs
@@ -9,8 +9,21 @@ namespace SLGame.Gameplay
     {
         [Header("References:")]
         [SerializeField] private PlayerMovement _playerMovement;
+
+        [Header("Stats: ")]
         [SerializeField] public float Gravity = -9.81f;
+
+        [Header("In game: ")]
         [SerializeField] public Vector3 Velocity;
+
+        [Space(10)]
+        [SerializeField] Transform GroundCheck;
+        [SerializeField] float groundDistance = 0.4f;
+        [SerializeField] LayerMask groundMask;
+
+        [SerializeField] private float StartingVelocityNumber = -2f;
+
+        [SerializeField] bool isGrounded;
         void Start()
         {
             this._playerMovement = this.gameObject.GetComponent<PlayerMovement>();
@@ -19,8 +32,22 @@ namespace SLGame.Gameplay
         // Update is called once per frame
         void Update()
         {
+            isGrounded = Physics.CheckSphere(GroundCheck.position, groundDistance, groundMask);
+
+            if (isGrounded && Velocity.y < 0)
+            {
+                Velocity.y = StartingVelocityNumber;
+            }
+
             Velocity.y += Gravity * Time.deltaTime;
             _playerMovement.CharacterController.Move(Velocity * Time.deltaTime);
+        }
+
+        void OnDrawGizmosSelected()
+        {
+            // Draw a yellow sphere at the transform's position
+            Gizmos.color = Color.red;
+            Gizmos.DrawSphere(GroundCheck.position, groundDistance);
         }
     }
 }
