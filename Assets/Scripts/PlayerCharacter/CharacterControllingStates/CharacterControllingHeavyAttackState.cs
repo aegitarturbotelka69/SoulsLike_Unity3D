f8ca3c1be0c453fcda2da899e0df32b060578ed9
Timur <jeynado_farbi_ocimio_eceterial_j@yahoo.com>
2022-08-10 08:11:57 +1000

    HeavyAttack state, event fix

---
+++ Assets/Scripts/PlayerCharacter/CharacterControllingStates/CharacterControllingHeavyAttackState.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using SLGame.Input;
+
+namespace SLGame.Gameplay
+{
+    public class CharacterControllingHeavyAttackState : CharacterControllingBaseState
+    {
+        public CharacterControllingHeavyAttackState(States enumState, PlayerMovement playerMovementReference, ref CharacterController controller)
+            : base(enumState, playerMovementReference, ref controller) { }
+
+        public override void Execute()
+        {
+            base.Execute();
+        }
+
+        public override void StartTransition()
+        {
+            //base.StartTransition();
+            //_playerMovement.CharacterAnimator.SetBool(States.Attack.ToString(), false);
+        }
+
+        public override void EndTransition(bool endingManually)
+        {
+            _playerMovement.CharacterAnimator.SetBool(States.HeavyAttack.ToString(), false);
+
+            if (!endingManually)
+                return;
+
+            _playerMovement.ChangeControllingState(States.Idle);
+            _playerMovement.CharacterAnimator.SetBool(States.Idle.ToString(), true);
+            //base.EndTransition(endingManually);
+        }
+    }
+}
